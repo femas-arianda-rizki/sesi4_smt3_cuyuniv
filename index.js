68012ca8c32738f67b6105e4b537883b4f8a690f
femas arianda <femasariandarizki@students.undip.ac.id>
2024-01-17 02:51:42 +0700

    commit all changes

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ const express = require("express");
 const app = express();
 const port = 3003;
 const bodyParser = require("body-parser");
+const db = require("./connection");
+const response = require("./response");
 
 app.use(bodyParser.json());
 
 // routes / URL / endpoint utama kita method GET
-app.get("/hello", (req, res) => {
-  console.log({ urlParam: req.query.address });
-  res.send("Hello World!");
+app.get("/find", (req, res) => {
+  console.log("find nim: ", req.query.nim);
+
+  const sql = `SELECT nama_lengkap FROM mahasiswa WHERE nim = ${req.query.nim}`;
+  db.query(sql, (error, result) => {
+    response(200, result, "find mahasiswa name", res)
+  })
 });
 
 app.get("/", (req, res) => {
-  res.send("yuhu!");
+  const sql = "SELECT * FROM mahasiswa";
+
+  db.query(sql, (error, result) => {
+    // hasil data dari mysql
+    response(200, result, "get all data from mahasiswa", res);
+  });
 });
 
 app.post("/login", (req, res) => {
